1df922246f4febadcd2a611fd1bc673b641d557d
biju46 <53279265+biju46@users.noreply.github.com>
2019-07-24 18:09:36 -0400

    Create Second.java

---
+++ Second.java
@@ -0,0 +1,13 @@
+package myfirstpro;
+
+public class Second {
+
+	static boolean aboolean;
+
+	public static void main(String[] args) {
+
+		System.out.println("the default value of long is " + aboolean);
+
+	}
+
+}
